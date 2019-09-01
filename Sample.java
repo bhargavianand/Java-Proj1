1b0e6538cde1597c3fbfbd0236c80e91e7a46b77
bhargavianand <bhargavianand@users.noreply.github.com>
2019-09-01 12:19:34 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Sample{
 public static void main(String args[]) throws InterruptedException {
 	System.out.println("Hello Java");
-	Thread.sleep(15000);
-	}
+	System.out.println("Changes made to File");
+}
 }
